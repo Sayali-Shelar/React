ce9c645ace0831cca2d3e39ad37942291b6647db
Sayali-Shelar <2005sayuu@gmail.com>
2025-09-22 19:05:35 +0530

    Add files via upload

---
+++ UseState/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+// import Practice from "./components/Practice";
+
+
+import Hero from "./components/Hero";
+import Maincard from "./components/Maincard";
+
+
+
+
+
+
+
+const App=(()=>{
+  return(
+    <div>
+      <Hero/>
+      <Maincard/>
+      {/* <Practice/> */}
+      
+  
+     
+      
+      
+    </div>
+  )
+})
+
+export default App;
