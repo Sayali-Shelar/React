555b334fa8e5f4727871b0f7bcc3a3769628f017
Sayali-Shelar <2005sayuu@gmail.com>
2025-09-23 17:13:55 +0530

    App.js

--- UseState/App.js
+++ UseState/App.js
@@ -1,28 +1,11 @@
 import React from "react";
-// import Practice from "./components/Practice";
-
-
-import Hero from "./components/Hero";
-import Maincard from "./components/Maincard";
-
-
-
-
-
-
+import Practice from "./components/Practice";
 
 const App=(()=>{
   return(
     <div>
-      <Hero/>
-      <Maincard/>
-      {/* <Practice/> */}
-      
-  
-     
-      
-      
-    </div>
+      <Practice/>
+     </div>
   )
 })
 
